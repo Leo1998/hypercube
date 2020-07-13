cf74b6bce4be881f4e74ed508322309e95df920d
Leo1998 <leonard.fricke98@gmail.com>
2020-07-13 10:46:58 +0200

    entering the fourth dimension

---
+++ base.py
@@ -0,0 +1,74 @@
+import numpy as np
+from math import *
+
+class HyperObject:
+  verts = []
+  edges = []
+  transform = np.identity(4)
+
+  def __init__(self, verts, edges):
+    self.verts = verts
+    self.edges = edges
+
+  def transformedVerts(self):
+    ts = []
+    for vert in self.verts:
+      ts.append(np.matmul(self.transform, vert))
+    return ts
+
+  def projectedVerts(self):
+    w_dist = 2
+    proj = []
+    transformed = self.transformedVerts()
+    for v in transformed:
+      w = 1 / (w_dist - v[3])
+      #projection matrix
+      m = np.array([[w, 0, 0, 0],
+                     [0, w, 0, 0],
+                     [0, 0, w, 0]])
+      proj.append(np.matmul(m, v))
+    return proj
+
+
+def generateUnitTesseract():
+  verts = []
+  for i in range(0, 2**4):
+    x = (i >> 0 & 1) * 2.0 - 1.0
+    y = (i >> 1 & 1) * 2.0 - 1.0
+    z = (i >> 2 & 1) * 2.0 - 1.0
+    w = (i >> 3 & 1) * 2.0 - 1.0
+    verts.append(np.array([x, y, z, w]))
+  return verts
+
+# edges are between points with unit distance
+def generateTesseractEdges(verts):
+  edges = []
+  for i0 in range(len(verts)):
+    for i1 in range(len(verts)):
+      if i0 != i1 and np.linalg.norm(verts[i0] - verts[i1]) == 2.0:
+        edges.append((i0, i1))
+  return edges
+
+class Tesseract(HyperObject):
+  def __init__(self):
+    verts = generateUnitTesseract()
+    edges = generateTesseractEdges(verts)
+    super(Tesseract, self).__init__(verts, edges)
+
+
+def rotateAroundPlane(ax1, ax2, angle):
+  assert(ax1 < ax2)
+  m = np.identity(4)
+  m[ax1][ax1] = cos(angle)
+  m[ax2][ax1] = sin(angle)
+  m[ax1][ax2] = -sin(angle)
+  m[ax2][ax2] = cos(angle)
+  return m
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,52 +1,30 @@
 import numpy
-from math import *
-from pyquaternion import Quaternion
 
+from pyquaternion import Quaternion
 import ctypes
 import pygame
 from pygame.locals import *
 import pyglet
 from pyglet.gl import *
 
-cube_verts = (
-    (-3.0,-3.0, 3.0),
-    (-3.0, 3.0, 3.0),
-    ( 3.0, 3.0, 3.0),
-    ( 3.0,-3.0, 3.0),
-    (-3.0,-3.0,-3.0),
-    (-3.0, 3.0,-3.0),
-    ( 3.0, 3.0,-3.0),
-    ( 3.0,-3.0,-3.0)
-)
+from base import *
 
-cube_edges = (
-    (0,1),
-    (0,3),
-    (0,4),
-    (2,1),
-    (2,3),
-    (2,6),
-    (5,1),
-    (5,4),
-    (5,6),
-    (7,3),
-    (7,4),
-    (7,6)
-)
 
-def Cube():
+def drawObject(obj):
+    proj = obj.projectedVerts()
+
     glPointSize(8.0)
     glBegin(GL_POINTS)
     glColor3fv((ctypes.c_float * 3)(*(1.0,1.0,1.0)))
-    for vert in cube_verts:
+    for vert in proj:
         glVertex3fv((ctypes.c_float * 3)(*vert))
     glEnd()
 
     glBegin(GL_LINES)
     glColor3fv((ctypes.c_float * 3)(*(0.8,0.8,0.8)))
-    for edge in cube_edges:
+    for edge in obj.edges:
         for vertex in edge:
-            glVertex3fv((ctypes.c_float * 3)(*cube_verts[vertex]))
+            glVertex3fv((ctypes.c_float * 3)(*proj[vertex]))
     glEnd()
 
 def main():
@@ -63,7 +41,10 @@ def main():
     gluPerspective(45, (display[0]/display[1]), 0.5, 40)
     glTranslatef(0.0,0.0,-17.5)
 
-    accum = Quaternion()
+    cube = Tesseract()
+
+    angle = 0
+    accum = Quaternion(1, 0, 0, 0)
     inc_x = 0
     inc_y = 0
 
@@ -99,7 +80,7 @@ def main():
 
         rot_x = Quaternion(axis=[1.0,0.0,0.0], angle=inc_x)
         rot_y = Quaternion(axis=[0.0,1.0,0.0], angle=inc_y)
-        accum = accum * rot_x * rot_y
+        accum = accum * rot_x * rot_y 
 
         # dont judge me pls
         mat = accum.transformation_matrix.T
@@ -110,7 +91,10 @@ def main():
 
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
 
-        Cube()
+        angle += pi/120
+        cube.transform = rotateAroundPlane(2, 3, angle)
+
+        drawObject(cube)
 
         pygame.display.flip()
         pygame.time.wait(10)
@@ -118,56 +102,3 @@ def main():
 if __name__ == '__main__':
   main()
 
-
-
-
-
-
-'''axis_verts = (
-    (-7.5, 0.0, 0.0),
-    ( 7.5, 0.0, 0.0),
-    ( 0.0,-7.5, 0.0),
-    ( 0.0, 7.5, 0.0),
-    ( 0.0, 0.0,-7.5),
-    ( 0.0, 0.0, 7.5)
-)
-
-axes = (
-    (0,1),
-    (2,3),
-    (4,5)
-)
-
-axis_colors = (
-    (1.0,0.0,0.0), # Red
-    (0.0,1.0,0.0), # Green
-    (0.0,0.0,1.0)  # Blue
-)
-
-cube_surfaces = (
-    (0,1,2,3), # Front
-    (3,2,6,7), # Right
-    (7,6,5,4), # Left
-    (4,5,1,0), # Back
-    (1,5,6,2), # Top
-    (4,0,3,7)  # Bottom
-)
-
-
-cube_colors = (
-    (0.769,0.118,0.227), # Red
-    (  0.0,0.318,0.729), # Blue
-    (  1.0,0.345,  0.0), # Orange
-    (  0.0, 0.62,0.376), # Green
-    (  1.0,  1.0,  1.0), # White
-    (  1.0,0.835,  0.0)  # Yellow
-)
-
-def Axis():
-    glBegin(GL_LINES)
-    for color,axis in zip(axis_colors,axes):
-        glColor3fv((ctypes.c_float * 3)(*color))
-        for point in axis:
-            glVertex3fv((ctypes.c_float * 3)(*axis_verts[point]))
-    glEnd()
-'''
